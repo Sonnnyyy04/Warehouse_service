410fd7851f4c0ec2b5aca8317305ad4029aea038
Sonnnyyy04 <sanseinikolai83@gmail.com>
2026-04-15 18:01:32 +1000

    fix product

--- internal/models/product.go
+++ internal/models/product.go
@@ -1,8 +1,8 @@
 package models
 
 type Product struct {
-	ID   int64
-	SKU  string
-	Name string
-	Unit string
+	ID   int64  `json:"id"`
+	SKU  string `json:"sku"`
+	Name string `json:"name"`
+	Unit string `json:"unit"`
 }
